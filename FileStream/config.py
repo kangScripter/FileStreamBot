63c39a56a949cef139d90c564cf5c5353b62904b
Teja <67798221+kangScripter@users.noreply.github.com>
2024-08-11 15:57:36 +0530

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -4,25 +4,25 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Telegram:
-    API_ID = int(env.get("API_ID"))
-    API_HASH = str(env.get("API_HASH"))
-    BOT_TOKEN = str(env.get("BOT_TOKEN"))
-    OWNER_ID = int(env.get('OWNER_ID', '7978482443'))
-    WORKERS = int(env.get("WORKERS", "6"))  # 6 workers = 6 commands at once
-    DATABASE_URL = str(env.get('DATABASE_URL'))
-    UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "Telegram"))
-    SESSION_NAME = str(env.get('SESSION_NAME', 'FileStream'))
+    API_ID = 5540967#int(env.get("API_ID"))
+    API_HASH = "eedf0196b0533f361b51b5b7082358e9"#str(env.get("API_HASH"))
+    BOT_TOKEN = "6030145995:AAGqawj9tdp6rAv6FSM6NonH6dbjKBX6rVc" #str(env.get("BOT_TOKEN"))
+    OWNER_ID = 1086048529#int(env.get('OWNER_ID', '7978482443'))
+    WORKERS = 10 #int(env.get("WORKERS", "6"))  # 6 workers = 6 commands at once
+    DATABASE_URL = "mongodb+srv://tej2leecher:aAnJNxbrhK7BHLIN@cachekeys.z4afwu7.mongodb.net/CacheKeys?retryWrites=true&w=majority" #str(env.get('DATABASE_URL'))
+    UPDATES_CHANNEL = "TeleXMovieZ"#str(env.get('UPDATES_CHANNEL', "Telegram"))
+    SESSION_NAME ="tnstreambot" #str(env.get('SESSION_NAME', 'FileStream'))
     FORCE_SUB_ID = env.get('FORCE_SUB_ID', None)
-    FORCE_SUB = env.get('FORCE_UPDATES_CHANNEL', False)
+    FORCE_SUB = True #env.get('FORCE_UPDATES_CHANNEL', False)
     FORCE_SUB = True if str(FORCE_SUB).lower() == "true" else False
     SLEEP_THRESHOLD = int(env.get("SLEEP_THRESHOLD", "60"))
     FILE_PIC = env.get('FILE_PIC', "https://graph.org/file/5bb9935be0229adf98b73.jpg")
     START_PIC = env.get('START_PIC', "https://graph.org/file/290af25276fa34fa8f0aa.jpg")
     VERIFY_PIC = env.get('VERIFY_PIC', "https://graph.org/file/736e21cc0efa4d8c2a0e4.jpg")
-    MULTI_CLIENT = False
-    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", None))   # Logs channel for file logs
-    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", None))   # Logs channel for user logs
-    MODE = env.get("MODE", "primary")
+    MULTI_CLIENT = True
+    FLOG_CHANNEL = "-1002184628163"#int(env.get("FLOG_CHANNEL", None))   # Logs channel for file logs
+    ULOG_CHANNEL = "-1002184628163" #int(env.get("ULOG_CHANNEL", None))   # Logs channel for user logs
+    MODE = env.get("MODE", "secondary")
     SECONDARY = True if MODE.lower() == "secondary" else False
     AUTH_USERS = list(set(int(x) for x in str(env.get("AUTH_USERS", "")).split()))
 
